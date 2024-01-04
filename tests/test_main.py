2ac8d32d24b8f402e89735013a61954c2bca54f2
mgreminger <michael.greminger@gmail.com>
2024-01-03 23:36:30 -0600

    tests: make sure github action fails on failed test

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,3 +26,5 @@ def test_md_to_docx():
         output_docx.write(response.content)
     
     assert filecmp.cmp('./tests/output/output.docx', './tests/output_reference.docx', shallow=False)
+
+    assert False
