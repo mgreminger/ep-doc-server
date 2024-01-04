b7ffd7584bcf9d91cfb4cd159d53351da8c35bda
mgreminger <michael.greminger@gmail.com>
2024-01-04 00:14:35 -0600

    tests: add test for pdf endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,13 +16,22 @@ def test_md_to_docx():
     response = client.post("/docgen/docx", files=files)
     assert response.status_code == 200
 
-    assert len(response.content) > 700000
-
-    # save output docx data to a file as an artifact
-    # can be compared to ./tests/output_reference.docx
-    # cannot use a bit-wise comparison since pandoc uses timestamp data
+    # save docx file as artifact
     os.makedirs('./tests/output', exist_ok=True)
     with open('./tests/output/output.docx', 'wb') as output_docx:
         output_docx.write(response.content)
     
     assert filecmp.cmp('./tests/output/output.docx', './tests/output_reference.docx', shallow=False)
+
+def test_md_to_pdf():
+    files = {'request_file': ("input.md", open('./tests/input.md', 'rb'), "text/markdown")}
+    response = client.post("/docgen/pdf", files=files)
+    assert response.status_code == 200
+
+    # save pdf file as artifact
+    os.makedirs('./tests/output', exist_ok=True)
+    with open('./tests/output/output.pdf', 'wb') as output_pdf:
+        output_pdf.write(response.content)
+    
+    # file comparison not working (change identifier metadata?) so will just check size
+    assert len(response.content) > 250000
