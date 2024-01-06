8ea8d19dc94de20ce243cdbf5f2c3afb6a2ea8d3
mgreminger <michael.greminger@gmail.com>
2024-01-06 14:05:17 -0600

    fix: fix bug where input md file wasn't being closed before calling pandoc

--- app/main.py
+++ app/main.py
@@ -63,21 +63,21 @@ async def convert_markdown_file(request_file: UploadFile, doc_type: Literal['doc
     with open(Path(temp_dir_name) / MD_FILE_NAME, 'wb') as md_input_file:
         await asyncio.to_thread(shutil.copyfileobj, request_file.file, md_input_file)
         
-        cmd = f"pandoc --from markdown --to {doc_type} --standalone --embed-resources --no-highlight {MD_FILE_NAME} -o {output_file_name}";
-        proc = await asyncio.create_subprocess_shell(
-            cmd=cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            cwd=temp_dir_name,
-            env=env)
+    cmd = f"pandoc --from markdown --to {doc_type} --standalone --embed-resources --no-highlight {MD_FILE_NAME} -o {output_file_name}";
+    proc = await asyncio.create_subprocess_shell(
+        cmd=cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=temp_dir_name,
+        env=env)
 
-        stdout, stderr = await proc.communicate()
+    stdout, stderr = await proc.communicate()
 
-        print(f'[{cmd!r} exited with {proc.returncode}]')
-        if stdout:
-            print(f'[stdout]\n{stdout.decode()}')
-        if stderr:
-            print(f'[stderr]\n{stderr.decode()}')
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
 
     return FileResponse(Path(temp_dir_name) / output_file_name,
                         media_type=MIME_TYPES[doc_type],
