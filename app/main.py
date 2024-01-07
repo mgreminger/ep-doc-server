aa2768102ab33580a9efaafe650e048ccbbd4ac1
mgreminger <michael.greminger@gmail.com>
2024-01-06 21:45:40 -0600

    feat: add error handling
    Check file size, enforce pandoc time limit, and check for successful pandoc return status

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ from fastapi.responses import FileResponse
 
 from pydantic_settings import BaseSettings
 
+SUBPROCESS_TIMEOUT = 10 # seconds
+MAX_INPUT_SIZE = 5000000 # bytes
 MD_FILE_NAME = "input.md"
 OUTPUT_FILE_NAME_BASE = "output"
 MIME_TYPES = {
@@ -39,7 +41,10 @@ async def delete_temp_directory(temp_dir_name):
 async def convert_markdown_file(request_file: UploadFile, doc_type: Literal['docx', 'pdf'],
                                 background_tasks: BackgroundTasks,
                                 settings: Annotated[Settings, Depends(get_settings)]):
-    
+    # make sure file is not too large
+    if request_file.size and (request_file.size > MAX_INPUT_SIZE):
+        raise HTTPException(status_code=413, detail="Sheet too large for document conversion, reduce size of images in documentation cells.")
+
     # verify file (protects against uploading a binary file, which will cause pandoc to hang)
     first_line = await request_file.read(43)
     try:
@@ -62,22 +67,22 @@ async def convert_markdown_file(request_file: UploadFile, doc_type: Literal['doc
 
     with open(Path(temp_dir_name) / MD_FILE_NAME, 'wb') as md_input_file:
         await asyncio.to_thread(shutil.copyfileobj, request_file.file, md_input_file)
-        
+    
     cmd = f"pandoc --from markdown --to {doc_type} --standalone --embed-resources --no-highlight {MD_FILE_NAME} -o {output_file_name}";
-    proc = await asyncio.create_subprocess_shell(
-        cmd=cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-        cwd=temp_dir_name,
-        env=env)
+    try:
+        proc = await asyncio.wait_for(asyncio.create_subprocess_shell(
+            cmd=cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=temp_dir_name,
+            env=env), SUBPROCESS_TIMEOUT)
+    except TimeoutError:
+        raise HTTPException(status_code=500, detail="Document Creation Timeout")
 
     stdout, stderr = await proc.communicate()
 
-    print(f'[{cmd!r} exited with {proc.returncode}]')
-    if stdout:
-        print(f'[stdout]\n{stdout.decode()}')
-    if stderr:
-        print(f'[stderr]\n{stderr.decode()}')
+    if proc.returncode != 0:
+        raise HTTPException(status_code=500, detail=stderr.decode())
 
     return FileResponse(Path(temp_dir_name) / output_file_name,
                         media_type=MIME_TYPES[doc_type],
